f437242b113aaadde9c51eb3d43f474d7f9e0e2c
Aaron Xavier <aarondxavier@gmail.com>
2017-04-22 13:19:04 -0700

    Now renders text from index.html and App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,13 @@
+import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      
-    };
-  }
-  updateVideos(video) {
-    this.setState({player: video});
   }
 
 
   render() {
     return (
-      <div>
-        <Nav />
-        <div className="col-md-7">
-          <VideoPlayer video={this.state.player}/>
-        </div>
-        <div className="col-md-5">
-          <VideoList videos={this.state.list} update={this.updateVideos.bind(this)}/>
-        </div>
-      </div>
+        <p>App</p>
     );
   }
 }
