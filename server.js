bf8167216b544592f2039707d6686ca9d9bf68f2
Aaron Xavier <aarondxavier@gmail.com>
2017-04-25 10:20:49 -0700

    remove DB info

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var router = express.Router();
 var port = process.env.API_PORT || 3000;
-mongoose.connect('mongodb://pepuser:pepuser1@ds049496.mlab.com:49496/pepclock');
+mongoose.connect('');
 var PepListItem = require('./models/pepListItem');
 
 app.use(bodyParser.urlencoded({ extended: true }));
